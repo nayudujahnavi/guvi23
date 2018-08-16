91ff30ab12e514e71f7c9c50dea78d76ebcbbe0b
nayudujahnavi <41770189+nayudujahnavi@users.noreply.github.com>
2018-08-16 11:54:48 +0530

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,6 +1,10 @@
 lower=int(input(" "))
 upper=int(input(" "))
 for i in range(lower +1,upper):
-# if(i%2!=0):
-#if(i%2==0):
+if(i%2!=0):
 print(i)
+lower=int(input(" "))
+upper=int(input(" "))
+for i in range(lower +1,upper):
+  if(i%2==0):
+    print(i)
