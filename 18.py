7d18264740d0963cfab93ff1c29ada652dc783be
nayudujahnavi <41770189+nayudujahnavi@users.noreply.github.com>
2018-08-17 10:05:28 +0530

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,9 @@
+lower = int(input("enter lower range: "))
+upper = int(input("enter upper range: "))
+for num in rnge(lower, upper + 1):
+sum =0
+temp=num
+while temp > 0:
+sum + =digit **3
+if num == sum
+print(num)
