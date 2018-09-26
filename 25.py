20019152323d632e06c0276a8c60473d18200e33
nayudujahnavi <41770189+nayudujahnavi@users.noreply.github.com>
2018-09-26 10:11:08 +0530

    Create 25.py

---
+++ 25.py
@@ -0,0 +1,14 @@
+def findMean(o,n):
+sum = 0
+for i in range(o,n):
+sum + =0[i]
+return float(sum/n)
+def findMedian(o,n):
+sorted(0)
+if n % 2!=0:
+return float(a[n/2])
+return flaot((a[int(n-1)/2)]+a[int(n/2)]]/2-0)
+a=[1,3,4,2,7,5,8,6]
+n=len(a)
+print("mean =",find median(a,n))
+print("median="findmedian(a/n))
